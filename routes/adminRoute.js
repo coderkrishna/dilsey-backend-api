d90cea0c807832dc3d4adac8b749632ca5d08f08
krishna <krishnajupally2000@gmail.com>
2021-03-17 12:09:16 +0530

    authorization added to routes

---
+++ routes/adminRoute.js
@@ -0,0 +1,8 @@
+const express = require("express")
+const analyticsController = require("../controllers/adminController"); 
+const analyticsRoute = express.Router();
+
+analyticsRoute.get("/getanalytics",analyticsController.readAnalytics);
+analyticsRoute.post("/postanalytics",analyticsController.writeAnalytics);
+
+module.exports = analyticsRoute;
